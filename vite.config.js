c465d70d1aaa61f0b06cc0a0cd1d115cdae6a0bf
Olcay Duzgun <88849954+Fuser2k@users.noreply.github.com>
2026-02-07 13:49:39 +0100

    Fix build memory issues and update vite config

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:3001',
-      '/uploads': 'http://localhost:3001'
+export default defineConfig(({ command, mode }) => {
+  return {
+    plugins: [react()],
+    server: {
+      proxy: command === 'serve' ? {
+        '/api': 'http://localhost:3001',
+        '/uploads': 'http://localhost:3001'
+      } : {}
+    },
+    build: {
+      chunkSizeWarningLimit: 1000,
     }
   }
 })
